36e779ddf50a152b52bf2a6d040caef453ba9488
Johannes 'fish' Ziemke <github@freigeist.org>
2018-10-30 10:00:42 +0100

    Use media query to switch column style

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,6 @@ class BlogIndex extends React.Component {
             paddingRight: index % 2 == 0 ? '0em' : rhythm(1),
           }
           // FIXME: Get rid of ugly workaround and use media queries
-          if (typeof window !== 'undefined' && window.innerWidth < 800) { style['width'] = '100%' }
           return (
             <div key={node.fields.slug} className={styles.preview} style={style}>
               <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
